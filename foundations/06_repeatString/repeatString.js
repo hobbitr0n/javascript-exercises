439e1b24758f6b32a812aca537c6a23d842591d4
Jimli <j.kachelries@gmail.com>
2026-06-10 12:28:43 -0400

    Looked at solution to understand what was being asked. Will attempt next lesson without peeking

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
+const repeatString = function (word, times) {
+    let result = "";
+    if (times < 0) return "ERROR";
+    for (let i = 0; i < times; i++) {
+        result += word;
+    };
+    console.log(result);
+    return result;
 
+    // for (let i = 0; i < times; i++) {
+    //     result += word;
+    // };
+    // console.log(result);
+    // return result;
 };
 
 // Do not edit below this line
