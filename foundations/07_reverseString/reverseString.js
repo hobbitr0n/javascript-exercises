ce41106f8032e9ebec9a2fa16ea3768da31e08d6
Jimli <j.kachelries@gmail.com>
2026-06-10 14:00:26 -0400

    Haven't looked at solution yet. I asked AI how I would retrieve the number of characters the user entered for a variable, and it told me about the .length -attribute?-. I wanted to use it as both the counter for the loop and using the same number to pull the character from the string.

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,4 +1,12 @@
-const reverseString = function() {
+const reverseString = function(reverse) {
+
+    let backwards = "";
+
+    for (let i = reverse.length - 1; i >= 0; i--) {
+        backwards = backwards + reverse[i];                
+    }
+
+    return backwards;
 
 };
 
