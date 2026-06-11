ff629f8431c63da89224cd20d8f528a0e85d25af
Jimli <j.kachelries@gmail.com>
2026-06-11 13:22:39 -0400

    Removed the remaining .skip and passed following tests. I suppose it was robust enough that no additional changes were needed, but I didn't read all the following tests requirements.

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -3,14 +3,12 @@ const removeFromArray = function(array, ...picker) {
     let options = [];
     options = picker;
 
-    console.log(options);
-
     for (let i = 0; i < array.length; i++) {
         if (!picker.includes(array[i])) {
             result.push(array[i]);
         }
     }
-
+    console.log(result);
     return result;
 };
 
