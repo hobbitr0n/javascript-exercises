a7311664a528085a9cd9181c1cc51b19ed6b0b62
Jimli <j.kachelries@gmail.com>
2026-06-11 13:02:47 -0400

    Passed first, stuck on rest for second

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, picker) {
+    let result = [];
+
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] !== picker) {
+            result.push(array[i]);
+        }
+    }
+
+    return result;
 };
 
 // Do not edit below this line
