26c99d5fa1d61bcdae00e8ab2b3ef4e821d201cf
Jimli <j.kachelries@gmail.com>
2026-06-11 16:00:54 -0400

    Taking a break. Goal is to either fill an array with all the numbers between 'one' and 'two' and add them together:  OR Add the numbers to a result via the loop and don't bother 'saving' all the numbers between 'one' and 'two'.

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,4 +1,41 @@
-const sumAll = function() {
+const sumAll = function(one, two) {
+    if (one < 0 || two < 0) {
+        return 'ERROR';
+    }
+
+    let ray = [];
+    let range = Math.abs(one - two);
+
+    // Setup ray before totalling everything up
+
+    for (let i = 0; i <= range; i++) {
+        
+    }
+
+
+
+    // Then break apart ray to add values together
+
+
+
+    if (two > one) {
+        for (let i = 0; i < dif; i++) {
+            ray.push(one + i);
+        }
+
+    }
+
+    if (one > two) {
+        result = two;
+        for (let i = 0; i <= range; i++) {
+            result += i;
+        }
+
+    }
+
+
+    console.log(ray);
+    return result;
 
 };
 
