5817ef871fa054d17422a288982ebda598e82bc6
Jimli <j.kachelries@gmail.com>
2026-06-17 10:15:50 -0400

    Passed 4, failed 2. Time to investigate.

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    // Divisible Constants
+    const divFour = year / 4;
+    const divHundred = year / 100;
+    const divFourHundred = divHundred / 400;
 
+    if (Number.isInteger(divFour) || Number.isInteger(divFourHundred)) {
+        return true;
+    }   else {
+        return false;
+    }
 };
 
 // Do not edit below this line
