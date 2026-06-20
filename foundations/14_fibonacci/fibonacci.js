14d2348b9186b692132d47ee482cfed0fcba1d41
Jimli <j.kachelries@gmail.com>
2026-06-20 13:57:18 -0400

    Not sure how effecient, will check solution.

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function(a) {
 
+    const num = Number(a);
+
+    console.log(num);
+
+    if (num < 0) {
+        return "OOPS";
+    }
+
+    if (num === 0)  {
+        return 0;
+    }
+
+    let fib = [1, 1];
+
+    for (let i = 0; i < (num - 1); i++) {
+        fib.push(fib[fib.length - 1] + fib[fib.length - 2]);
+    }
+
+    console.log(fib[num - 1]);
+    return fib[num - 1];
 };
 
 // Do not edit below this line
