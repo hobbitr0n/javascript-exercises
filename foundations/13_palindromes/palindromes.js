0fe9114f9c9f4c99d32330646abaf702efa26d0d
Jimli <j.kachelries@gmail.com>
2026-06-20 10:57:13 -0400

    Created filter to remove special characters and loop to reverse the cleaned up string.

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,28 @@
-const palindromes = function () {
+const palindromes = function (word) {
 
+    const filter = [" ", "", ",", ".", "!"];
+    let wordless = "";
+    let wordlessReversed = "";
+
+    for (let i = 0; i < word.length; i++)   {
+        if (!filter.includes(word[i]))
+            wordless += word[i];
+    }
+    
+    for (let i = wordless.length; i > 0; i--)   {
+        wordlessReversed += wordless[i - 1];
+    }
+
+    console.log(word);
+    console.log(wordless);
+    console.log(wordlessReversed);
+
+
+    if (wordless === wordlessReversed)  {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
