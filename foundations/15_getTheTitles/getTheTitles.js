5a4670f08d6fcaab2c52b69518be5094f9c286ea
Jimli <j.kachelries@gmail.com>
2026-06-22 11:43:08 -0400

    Breakthrough with including array indexes on the left-hand side within the for loop. Going to check solution now.

--- foundations/15_getTheTitles/getTheTitles.js
+++ foundations/15_getTheTitles/getTheTitles.js
@@ -1,5 +1,12 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    let result = [];
+    for (let i = 0; i < books.length; i++) {
+        result[i] = books[i].title;
+        console.log(result);
+    }
+    console.log(typeof books);
+    console.log(typeof result);
+    return result;
 };
 
 // Do not edit below this line
