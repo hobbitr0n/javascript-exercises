a042d25d6907710c937a6022f14a0e6c6409541b
Jimli <j.kachelries@gmail.com>
2026-06-23 10:52:17 -0400

    Finished assignment, going to check solution.

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,25 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    const today = new Date();
+    let age = [];
+    let oldest = 0;
+    let person = 0;
 
+    for (let i = 0; i < array.length; i++)  {
+        if (array[i].yearOfDeath === undefined) {
+            age[i] = today.getFullYear() - array[i].yearOfBirth;
+        } else {
+            age[i] = array[i].yearOfDeath - array[i].yearOfBirth;
+        }
+    }
+
+    for (let i = 0; i < age.length; i++)    {
+        if (age[i] > oldest)    {
+            oldest = age[i];
+            person = i;
+        }
+    }
+
+    return array[person]; 
 };
 
 // Do not edit below this line
